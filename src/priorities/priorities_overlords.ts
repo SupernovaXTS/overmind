e72c2f12ad9a211c9b3e0b5216130745d63e399c
SupernovaXTS <dylanhermangamer@gmail.com>
2025-10-19 13:16:05 -0400

    controller attack tweaks

--- src/priorities/priorities_overlords.ts
+++ src/priorities/priorities_overlords.ts
@@ -31,7 +31,7 @@ export let OverlordPriority = {
 		destroy         : 300,
 		healPoint       : 301,
 		siege           : 302,
-		controllerAttack: 399,
+		controllerAttack: 402, // This should be lower then claiming unclaimed rooms as it takes longer
 	},
 	
 	colonization: { 			// Colonizing new rooms
