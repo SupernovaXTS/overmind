74b56324f14bc80e9cb15b09708a1aef0d099096
SupernovaXTS <dylanhermangamer@gmail.com>
2025-10-19 19:03:02 -0400

    oh look colony time

--- dist/main.js
+++ dist/main.js
@@ -5877,6 +5877,11 @@ let OverlordPriority = {
         siege: 302,
         controllerAttack: 399,
     },
+    colonization: {
+        claim: 400,
+        pioneer: 403,
+        remoteUpgrading: 610,
+    },
     priorityOwnedRoom: {
         priorityUpgrade: 450,
         priorityTransport: 451,
@@ -5888,11 +5893,6 @@ let OverlordPriority = {
         mineralRCL8: 503,
         transport: 504,
         mineral: 505,
-    },
-    colonization: {
-        claim: 401,
-        pioneer: 551,
-        remoteUpgrading: 552,
     },
     outpostOffense: {
         harass: 560,
