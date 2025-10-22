6a49173e805a4d922c65b72f58db868b097b495c
SupernovaXTS <Dylanhermangamer@gmail.com>
2025-10-22 15:03:28 -0400

    dont recharge workers during bootstrapping

--- src/tasks/instances/recharge.ts
+++ src/tasks/instances/recharge.ts
@@ -42,9 +42,11 @@ export class TaskRecharge extends Task<rechargeTargetType> {
 				}
 			}
 		}
-
+		if ( creep.roleName == 'worker' || creep.colony?.state.bootstrapping ) {
+			return false; // while bootstrapping do not allow workers to withdraw energy
+		}
 		// Don't allow workers to withdraw from mining containers at lower levels
-		if ((creep.colony && creep.colony.stage == ColonyStage.Larva && creep.roleName == 'worker') || creep.colony?.state.bootstrapping) {
+		if (creep.colony && creep.colony.stage == ColonyStage.Larva && creep.roleName == 'worker')) {
 			const miningSiteContainers = _.compact(_.map(creep.colony.miningSites, site => site.overlords.mine.container));
 			// if this is a mining site container, don't let creeps withdraw from it unless it is sort of full
 			const CONTAINER_THRESHOLD = 1000;
