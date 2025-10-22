8eea761cdaaf1e63868c3d0eb653d350bb2aa8fd
SupernovaXTS <dylanhermangamer@gmail.com>
2025-10-22 06:49:03 -0400

    adds more

--- src/overlords/situational/bootstrap.ts
+++ src/overlords/situational/bootstrap.ts
@@ -105,7 +105,7 @@ export class BootstrappingOverlord extends Overlord {
 			} else {
 				
 				// wish for rcl/2 fillers
-				var fillerAmt = 1 + Math.ceil(this.colony.controller.level / 2);
+				var fillerAmt = 1 + Math.ceil(this.colony.controller.level);
 				this.wishlist(fillerAmt, Setups.filler);
 			}
 		}
