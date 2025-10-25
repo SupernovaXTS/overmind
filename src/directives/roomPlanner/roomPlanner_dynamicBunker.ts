2538a2c6f32f8fdb8dbd929978d9fc213f8e6fab
SupernovaXTS <dylanhermangamer@gmail.com>
2025-10-24 23:58:29 -0400

    Change dynamic bunker directive back to evolution chamber for manual placement

--- src/directives/roomPlanner/roomPlanner_dynamicBunker.ts
+++ src/directives/roomPlanner/roomPlanner_dynamicBunker.ts
@@ -1,16 +1,16 @@
-import {bunkerLayout} from '../../roomPlanner/layouts/bunker';
+import {evolutionChamberLayout} from '../../roomPlanner/layouts/evolutionChamber';
 import {profile} from '../../profiler/decorator';
 import {Visualizer} from '../../visuals/Visualizer';
 import {Directive} from '../Directive';
 
 /**
- * Manually place a dynamic bunker core anchored at the target location for the RoomPlanner to use in semiautomatic or manual mode.
- * The evolution chamber must be placed separately away from the bunker.
+ * Manually place an evolution chamber anchored at the target location for the RoomPlanner to use in semiautomatic or manual mode.
+ * This enables dynamic bunker planning - the bunker core must be placed separately with a standard bunker directive.
  */
 @profile
 export class DirectiveRPDynamicBunker extends Directive {
 
-	static directiveName = 'roomPlanner:DynamicBunker';
+	static directiveName = 'roomPlanner:EvolutionChamber';
 	static color = COLOR_WHITE;
 	static secondaryColor = COLOR_CYAN;
 
@@ -23,7 +23,7 @@ export class DirectiveRPDynamicBunker extends Directive {
 	}
 
 	init(): void {
-		this.colony.roomPlanner.addComponent('bunker', this.pos, this.memory.rotation);
+		this.colony.roomPlanner.addComponent('evolutionChamber', this.pos, this.memory.rotation);
 	}
 
 	run(): void {
@@ -31,6 +31,6 @@ export class DirectiveRPDynamicBunker extends Directive {
 	}
 
 	visuals(): void {
-		Visualizer.drawLayout(bunkerLayout, this.pos);
+		Visualizer.drawLayout(evolutionChamberLayout, this.pos);
 	}
 }
